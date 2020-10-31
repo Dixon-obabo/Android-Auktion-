30161865c59c9cf6e82f777da0737979e8b98ce7
Dixon-obabo <cheche.oloo@gmail.com>
2020-10-31 16:22:43 +0300

    just some changes

--- app/src/main/java/com/example/auktion/MainActivity.java
+++ app/src/main/java/com/example/auktion/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.auktion;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +10,9 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -38,6 +43,41 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Hello:)", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+
+    ChildEventListener childEventListener= new ChildEventListener() {
+    @Override
+    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+    }
+
+    @Override
+    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+    }
+
+    @Override
+    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+    }
+
+    @Override
+    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError error) {
+
+    }
+};
+
+
+    public void getdate(){
+        dbref.addChildEventListener(childEventListener);
+    }
+
 
 
 }
