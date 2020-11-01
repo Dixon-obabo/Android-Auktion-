bcfb7fb5841d1dbc88bf3178da00a27ef7032cd8
Dixon-obabo <cheche.oloo@gmail.com>
2020-11-01 16:30:39 +0300

    Login chaenges

--- app/src/main/java/com/example/auktion/login.java
+++ app/src/main/java/com/example/auktion/login.java
@@ -3,12 +3,40 @@ package com.example.auktion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
+    EditText name;
+    EditText phone;
+    EditText email;
+    EditText password;
+    TextView sigin;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        name=findViewById(R.id.name);
+        phone=findViewById(R.id.phone);
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        login=findViewById(R.id.login);
+        sigin=findViewById(R.id.signin);
+    }
+
+    public void show(View view) {
+
+        phone.setVisibility(View.VISIBLE);
+        name.setVisibility(View.VISIBLE);
+
+    }
+
+    public void login(View view) {
+
+
     }
 }
