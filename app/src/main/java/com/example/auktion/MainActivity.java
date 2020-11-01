9c8509c5af3ca0193cafdb7943d716cff2e6c1a9
Dixon-obabo <cheche.oloo@gmail.com>
2020-11-01 09:02:50 +0300

    just some changes

--- app/src/main/java/com/example/auktion/MainActivity.java
+++ app/src/main/java/com/example/auktion/MainActivity.java
@@ -10,72 +10,74 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.ChildEventListener;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.storage.FirebaseStorage;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth mauth;
-    FirebaseDatabase database;
-    DatabaseReference dbref;
-    FirebaseUser currentuser;
-    private FirebaseStorage storage;
+//    FirebaseAuth mauth;
+//    FirebaseDatabase database;
+//    DatabaseReference dbref;
+//    FirebaseUser currentuser;
+//    private FirebaseStorage storage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    currentuser=mauth.getCurrentUser();
-    dbref=database.getReference("posts");
-    Login_status();
+//    currentuser=mauth.getCurrentUser();
+//    dbref=database.getReference("posts");
+//    Login_status();
+//
     }
 
     public void Login_status(){
-        if(currentuser==null){
-            Intent intent= new Intent(getApplicationContext(),login.class);
-            startActivity(intent);
-        }else {
-            Toast.makeText(this, "Hello:)", Toast.LENGTH_SHORT).show();
-        }
+//        if(currentuser==null){
+//            Intent intent= new Intent(getApplicationContext(),login.class);
+//            startActivity(intent);
+//        }else {
+//            Toast.makeText(this, "Hello:)", Toast.LENGTH_SHORT).show();
+//        }
     }
 
 
 
-    ChildEventListener childEventListener= new ChildEventListener() {
-    @Override
-    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-    }
-
-    @Override
-    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-    }
-
-    @Override
-    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-    }
-
-    @Override
-    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-    }
-
-    @Override
-    public void onCancelled(@NonNull DatabaseError error) {
-
-    }
-};
+//    ChildEventListener childEventListener= new ChildEventListener() {
+//    @Override
+//    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//
+//    }
+//
+//    @Override
+//    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//
+//    }
+//
+//    @Override
+//    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+//
+//    }
+//
+//    @Override
+//    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//
+//    }
+//
+//    @Override
+//    public void onCancelled(@NonNull DatabaseError error) {
+//
+//    }
+//};
 
 
     public void getdate(){
-        dbref.addChildEventListener(childEventListener);
+//        dbref.addChildEventListener(childEventListener);
+
     }
 
 
