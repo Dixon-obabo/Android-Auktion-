2b2ccc7b3e5427eae663f79b68940ccc3091e7fd
Dixon-obabo <cheche.oloo@gmail.com>
2020-11-02 09:36:31 +0300

    starting bidasset activity

--- app/src/main/java/com/example/auktion/Bidasset.java
+++ app/src/main/java/com/example/auktion/Bidasset.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Bidasset extends AppCompatActivity {
@@ -13,6 +16,9 @@ public class Bidasset extends AppCompatActivity {
     String asset_key;
     String asset_desc;
     String date;
+    String mail;
+    TextView name,price,desc,email,det;
+    EditText nbid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,17 @@ public class Bidasset extends AppCompatActivity {
         asset_key=intent.getStringExtra("key");
         asset_desc=intent.getStringExtra("asset_description");
         date=intent.getStringExtra("date");
+        mail=intent.getStringExtra("email");
+        name=findViewById(R.id.name);
+        price=findViewById(R.id.oldbid);
+        desc=findViewById(R.id.description);
+        email=findViewById(R.id.email);
+        det=findViewById(R.id.date);
+        nbid=findViewById(R.id.newbid);
         Toast.makeText(this, asset_desc+asset_name, Toast.LENGTH_SHORT).show();
+    }
+
+    public void postbid(View view) {
+
     }
 }
