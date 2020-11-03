eccb74ab50e89775ae4d7dc1b4cdf50a7c4f70a3
Dixon-obabo <cheche.oloo@gmail.com>
2020-11-03 16:50:08 +0300

    new bid eventlistener

--- app/src/main/java/com/example/auktion/Bidasset.java
+++ app/src/main/java/com/example/auktion/Bidasset.java
@@ -1,5 +1,7 @@
 package com.example.auktion;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -16,6 +18,9 @@ import com.bumptech.glide.load.resource.bitmap.FitCenter;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -67,7 +72,7 @@ public class Bidasset extends AppCompatActivity {
         nbid=findViewById(R.id.newbid);
        // Toast.makeText(this, asset_desc+asset_name, Toast.LENGTH_SHORT).show();
         putdata();
-
+        getnewbid();
     }
 
     public void postbid(View view) {
@@ -118,5 +123,38 @@ public class Bidasset extends AppCompatActivity {
         });
     }
 
+
+    public void getnewbid(){
+        FirebaseDatabase.getInstance().getReference("bids").child(asset_key).addChildEventListener(newbid);
+    }
+
+    ChildEventListener newbid=new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            String []data=snapshot.getValue().toString().split(",");
+
+            price.setText(data[2].replace("bid=","")+" Ksh");
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    };
 
 }
